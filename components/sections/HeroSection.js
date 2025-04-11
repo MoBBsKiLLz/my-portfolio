de6607bc11feef40bc7a2692e447e3436a48f0c1
MoBBsKiLLz <migg_18@live.com>
2025-04-10 17:25:03 -0700

    Updated heroimage quality

--- components/sections/HeroSection.js
+++ components/sections/HeroSection.js
@@ -2,7 +2,7 @@ export default function HeroSection() {
   return (
     <div
       className="h-screen bg-cover bg-center relative flex items-center justify-center"
-      style={{ backgroundImage: "url('/images/heroimage.png')" }}
+      style={{ backgroundImage: "url('/images/heroimage1.png')" }}
     >
       <div className="absolute inset-0 bg-orange-400/60" />
     </div>
